07e844bea4e6f9b3dc666e6cf40554448dc2ffbd
Grace Chiang <gracechiang@me.com>
2018-04-08 04:35:42 +0000

    commiting before pulling

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ source 'https://rubygems.org'
 # end
 
 gem "react-rails"
-gem 'webpacker', git: 'https://github.com/rails/webpacker.git'
+gem 'webpacker', '~> 3.4'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.6'
 # Use sqlite3 as the database for Active Record
--- app/javascript/components/Profile.js
+++ app/javascript/components/Profile.js
@@ -1,18 +1,28 @@
 import React, {Component} from 'react';
 import {LineChart, Line} from 'recharts';
+import { Row, Col, Input, Button, Card, CardTitle, CardPanel } from 'react-materialize';
 
 class Profile extends Component {
   render() {
     return (
-      <form> 
-        <label>Name:
-          <input type="text" name="name" />
-        </label>
-          <label>Email:
-            <input type="email" name="email" />
-        </label> 
-          <input type = "submit" value = "Submit" /> 
-      </form>
+      <Col className="profileCard" m={6} s={12}>
+        <Card className="profileContent" title="Profile">
+            <Input s={6} label="Nickname" validate defaultValue='Alvin' alt="nickname" /> 
+            <Input s={6} label="Email" validate defaultValue='alvin@alvin.com' alt="email" />
+              <div>
+                <Button waves='light'>Update Change</Button>
+              </div>
+        </Card>
+      </Col>
+      //   <div class>
+      //     <Row> 
+      //       <Input s={6} label="Nickname" validate defaultValue='Alvin' alt="nickname" /> 
+      //       <Input s={6} label="Email" validate defaultValue='alvin@alvin.com' alt="email" />
+      //         <div> 
+      //           <Button waves='light'>Update Change</Button>
+      //         </div>
+      //     </Row>
+      //  </div>
     );
   }  
 }
