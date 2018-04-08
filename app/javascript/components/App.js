da2d5fa078cf5f80f16ab72b7c0a374ffde4868b
Di Wu <wd1991610@gmail.com>
2018-04-07 17:52:51 -0700

    refactored my code 2 less files
    killed AppContainer.js and Main.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,16 +1,27 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+import ReactDOM from 'react-dom'
 import React from "react"
 import PropTypes from "prop-types"
 import Navbar from "./Navbar"
-import Main from "./Main"
 import Footer from "./Footer"
+import Profile from "./Profile"
+import Home from "./Home"
+import User from "./User"
+import Test from "./Test"
+
 class App extends React.Component {
   render () {
     return (
-      <React.Fragment>
-        <Navbar/>
-        <Main/>
-        <Footer/>
-      </React.Fragment>
+        <BrowserRouter>
+          <div>
+            <Navbar/>
+              <Route exact path='/' component={Home} />
+              <Route path='/profile' component={Profile} />
+              <Route path='/user' component={User} />
+              <Route path='/test' component={Test} />
+            <Footer/>
+          </div>
+        </BrowserRouter>
     );
   }
 }
