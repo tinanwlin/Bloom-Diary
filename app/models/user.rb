bfa656e7afa4550627b747acfb27293e5255c64d
tinanwlin <tinanwlin@gmail.com>
2018-04-08 08:11:54 +0000

    Complete user validation and successfully test user register/login/logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,23 @@
 class User < ActiveRecord::Base
+    before_save { self.email = email.downcase } 
+
     has_many :journals
 
-    # has_secure_password
+    has_secure_password
 
-    validates :nickname, :email, :password, presence: true
+    validates :nickname, :email, :password, :password_confirmation, presence: true
     validates :email, uniqueness: { case_sensitive: false }
     validates :password, length: { minimum: 4 }
     
+
+    def self.authenticate_with_credentials email, password
+      email.strip!
+      email.downcase!
+      if user = User.find_by(email: email).try(:authenticate, password)
+        user
+      else
+        nil
+      end
+    end
+
 end
