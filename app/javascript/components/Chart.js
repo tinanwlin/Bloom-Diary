90cbf2e96cb6fd069fb0eb0decb481b05d917321
Grace Chiang <gracechiang@me.com>
2018-04-10 19:08:17 -0700

    changed chart so now it's transparent

--- app/javascript/components/Chart.js
+++ app/javascript/components/Chart.js
@@ -2,25 +2,6 @@ import React, {Component} from 'react';
 var RadarChart = require("react-chartjs").Radar;
 
 
-// import Chart from 'chart.js';
-
-// Chart.defaults.global.animation.duration = 200;
-
-// const radarChartTest = new Chart.Radar(CHART, {
-//   type: 'radar',
-//   data: {
-//     labels : ['joy', 'anger', 'disgust', 'sadness', 'fear'], 
-//     datasets: [
-//       {
-//         scores: [0.47, 0.14, 0.13, 0.5, 0.09]
-//       }
-//     ]
-//   }
-// });
-
-
-
-
 class MyComponent extends Component {
   render() {
     return (
@@ -28,25 +9,24 @@ class MyComponent extends Component {
       labels : ['joy', 'anger', 'disgust', 'sadness', 'fear'],
       datasets: [
         {
-          label: '# of Votes',
-          data : [0.47, 0.14, 0.13, 0.5, 0.09],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
+          label: "Yesterday",
+          fillColor: "rgba(136, 135, 255,0.2)",
+          strokeColor: "rgba(136, 135, 255,1)",
+          pointColor: "rgba(136, 135, 255,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(136, 135, 255,1)",
+          data: [0.47, 0.14, 0.13, 0.5, 0.09]
+        },
+        {
+          label: "Today",
+          fillColor: "rgba(151,187,205,0.2)",
+          strokeColor: "rgba(151,187,205,1)",
+          pointColor: "rgba(151,187,205,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(151,187,205,1)",
+          data: [0.59, 0.10, 0.04, 0.12, 0.08]
         }
       ]
     }}
